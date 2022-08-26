9c7323f1865c84517b3ce2b11f35699e3e4b18f4
nirvanabaid <96126146+nirvanabaid@users.noreply.github.com>
2022-08-26 15:05:17 +0530

    jdsiownndx

--- lib/Screens/Home/helpMainPage.dart
+++ lib/Screens/Home/helpMainPage.dart
@@ -1,5 +1,7 @@
+import 'package:dronaidapp/components/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class HelpPage extends StatefulWidget {
   @override
@@ -24,19 +26,40 @@ class _HelpPageState extends State<HelpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        initialCameraPosition: _initialCameraPosition,
-        onMapCreated: (controller) => _googleMapController,
+      body: SlidingUpPanel(
+        backdropEnabled: true,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
+        panel: _createListMenu(), color: Colors.blueGrey, backdropColor: Colors.blue, backdropTapClosesPanel: true,
+
+        body: GoogleMap(
+          myLocationButtonEnabled: false,
+          zoomControlsEnabled: false,
+          initialCameraPosition: _initialCameraPosition,
+          onMapCreated: (controller) => _googleMapController,
+        ),
+        collapsed: Container(decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0))),
+        child: Center(child: Text('Slide Upwards', style: TextStyle(color: Colors.white),),),),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.black12,
+        backgroundColor: Colors.black54,
+        foregroundColor: Colors.blue,
         onPressed: () => _googleMapController?.animateCamera(
             CameraUpdate.newCameraPosition(_initialCameraPosition)),
         child: const Icon(Icons.center_focus_strong),
       ),
     );
   }
+
+ Widget _createListMenu() {
+    return Container(margin: const EdgeInsets.only(top: 36.0), color: Colors.white,
+      child:Row(
+        children: [
+
+        ],
+      )
+
+
+    );
+
+ }
 }
