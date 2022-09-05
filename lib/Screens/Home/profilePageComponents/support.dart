e03f4028079f1e5a395e5144606b01d51cde73bc
harshb-15 <hbhansali007@gmail.com>
2022-09-05 19:36:07 +0530

    harsh

--- lib/Screens/Home/profilePageComponents/support.dart
+++ lib/Screens/Home/profilePageComponents/support.dart
@@ -1,4 +1,6 @@
+import 'package:dronaidapp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Support extends StatefulWidget {
   static const String id = "support";
@@ -33,6 +35,52 @@ class _SupportState extends State<Support> {
       ),
       resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xFFF2F3FC),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        children: [
+          Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/supportPage.jpg'),
+              ),
+              GestureDetector(
+                onTap: (){
+                  print("On tap called");
+                  launchUrl(Uri.parse("tel:9484666168"));
+                },
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.call,
+                      size: 30,
+                      color: Color(0xff00078B),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Phone Number",
+                          style: blueTextStyle,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "+91 9484666168",
+                          style: lightBlueTextStyle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
