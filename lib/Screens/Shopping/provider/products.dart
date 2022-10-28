ae496320680e58d2badc38df914608df15960bce
Sandeep Kumar Rai <srai15032001@gmail.com>
2022-10-28 11:54:29 +0530

    Databse fetching Added

--- lib/Screens/Shopping/provider/products.dart
+++ lib/Screens/Shopping/provider/products.dart
@@ -1,18 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'product.dart';
+import 'package:http/http.dart' as http;
 
 // Here Product class is using mixin Chage Notifier
 class Products with ChangeNotifier{
-   List<Product> _items = [
-    Product(id: 'p1', title: 'Red shirt', description: 'A red shirt', price: 45245, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-    Product(id: 'p2', title: 'white shirt', description: 'This is a wite shirt', price: 63556, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-    Product(id: 'p3', title: 'Pink shirt', description: 'This is a wite shirt', price: 63556, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-    Product(id: 'p4', title: 'Blue shirt', description: 'This is a wite shirt', price: 63556, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-    Product(id: 'p5', title: 'Yellow shirt', description: 'This is a wite shirt', price: 63556, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-    Product(id: 'p6', title: 'Green shirt', description: 'This is a wite shirt', price: 63556, imageUrl: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'),
-  ];
+   List<Product> _items = [];
 
   var _showFavouriteOnly = false;
+  var _isinit = true;
 
   // this is the getter which is getting the data
   List<Product> get items{
@@ -27,6 +24,25 @@ class Products with ChangeNotifier{
   Product findById(String id){
     return _items.firstWhere((element) => element.id == id);
   }
+
+  Future<void> fetchAndSetProduct()async{
+        const url = 'https://project-dronaid-480ca-default-rtdb.firebaseio.com/INVENTORY.json';
+        try{
+          final response  = await http.get(Uri.parse(url));
+          final extractedData = json.decode(response.body) as Map<String, dynamic>;
+          print(extractedData);
+          final List<Product> onlineList = [];
+          extractedData.forEach((key, value) {
+            onlineList.add(Product(id: value["id"].toString(), title: value["name"].toString() , description: value["category"].toString(), imageUrl: value["url"]));
+          });
+
+          _items = onlineList;
+          notifyListeners();
+        }
+        catch(e){
+          print('Error occured');
+        };
+   }
 
   void addProduct(){
     //  _items.add(value)
